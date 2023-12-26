b0d5b1156336e9b23cbca955961e2c6a2a305dae
vc743 <victorcontrerasvargas55@gmail.com>
2023-12-25 20:14:10 -0400

    Menu lateral actualizado

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 /* Este código maneja la funcionalidad de filtrado de las tarjetas en la página web. */
 const cardContainers = document.querySelectorAll(".col-12");
 const menuLinks = document.querySelectorAll(".nav-menu-link");
+const offcanvas = new bootstrap.Offcanvas(document.getElementById('offcanvasNavbar'));
 
 menuLinks.forEach(link => {
   link.addEventListener("click", function() {
+
+    // Cierra el menu lateral al hacer clic en un enlace
+    offcanvas.hide();
+
     const filterText = this.textContent; // Obteniendo el texto del botón
 
     cardContainers.forEach(card => {
